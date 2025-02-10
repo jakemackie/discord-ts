e83910bbfda54193c869ccc04d47dadb65ebfe72
jakemackie <jake.wmackie@gmail.com>
2025-02-10 21:32:37 +0000

    feat: avatar command with interaction options

---
+++ src/commands/misc/avatar.ts
@@ -0,0 +1,32 @@
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import type { ChatInputCommandInteraction } from 'discord.js';
+
+export default {
+  category: 'misc',
+  data: new SlashCommandBuilder()
+    .setName('avatar')
+
+    .setDescription('Get the avatar of a user')
+    .addUserOption((option) =>
+      option.setName('user').setDescription('The user to get the avatar of')
+    ),
+  async execute(interaction: ChatInputCommandInteraction) {
+    const target = interaction.options.getUser('user') ?? interaction.user;
+
+    const avatarEmbed = new EmbedBuilder()
+      .setAuthor({
+        name: interaction.user.username,
+        iconURL: interaction.user.displayAvatarURL()
+      })
+      .setDescription(`[${target.username}](${target.displayAvatarURL()})`)
+
+      .setImage(
+        target.displayAvatarURL({
+          size: 1024
+        })
+      )
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [avatarEmbed] });
+  }
+};
--- src/register.ts
+++ src/register.ts
@@ -58,3 +58,29 @@ const rest = new REST().setToken(token);
     console.error(error);
   }
 })();
+
+// or delete command(s)
+
+// singular
+
+// for guild-based commands
+// rest.delete(Routes.applicationGuildCommand(clientId, guildId, 'commandId'))
+// 	.then(() => console.log('Successfully deleted guild command'))
+// 	.catch(console.error);
+
+// for global commands
+// rest.delete(Routes.applicationCommand(clientId, 'commandId'))
+// 	.then(() => console.log('Successfully deleted application command'))
+// 	.catch(console.error);
+
+// all
+
+// for guild-based commands
+// rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+// 	.then(() => console.log('Successfully deleted all guild commands.'))
+// 	.catch(console.error);
+
+// for global commands
+// rest.put(Routes.applicationCommands(clientId), { body: [] })
+// 	.then(() => console.log('Successfully deleted all application commands.'))
+// 	.catch(console.error);
