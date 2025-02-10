378d3000bce21dd14f543bf4ca78b1d65486d425
jakemackie <jake.wmackie@gmail.com>
2025-02-10 23:49:08 +0000

    feat: prisma setup

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,5 +1,6 @@
 import { Events, Interaction, MessageFlags } from 'discord.js';
 import { ExtendedClient } from '../index';
+import { prisma } from '../../lib/prisma';
 
 export default {
   name: Events.InteractionCreate,
@@ -18,6 +19,24 @@ export default {
     }
 
     try {
+      let userInDatabase = await prisma.user.findUnique({
+        where: {
+          discordId: interaction.user.id
+        }
+      });
+
+      if (!userInDatabase) {
+        userInDatabase = await prisma.user.create({
+          data: {
+            discordId: interaction.user.id,
+            discordUsername: interaction.user.username,
+            discordDisplayName: interaction.user.displayName
+          }
+        });
+      }
+
+      console.log(userInDatabase);
+
       await command.execute(interaction);
     } catch (error) {
       console.error(error);
