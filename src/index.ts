65c937361f7de9138a702902cc89472b11c55333
jakemackie <jake.wmackie@gmail.com>
2025-02-10 18:54:51 +0000

    feat: docs load files

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,80 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
+import {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  MessageFlags
+} from 'discord.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const token: string = process.env.DISCORD_CLIENT_TOKEN;
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// Create a new extended client instance
+interface ExtendedClient extends Client {
+  commands: Collection<string, any>;
+}
+
+const client: ExtendedClient = new Client({
+  intents: [GatewayIntentBits.Guilds]
+}) as ExtendedClient;
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
+    }
+  }
+}
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = (interaction.client as ExtendedClient).commands.get(
+    interaction.commandName
+  );
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: 'There was an error while executing this command!',
+        flags: MessageFlags.Ephemeral
+      });
+    } else {
+      await interaction.reply({
+        content: 'There was an error while executing this command!',
+        flags: MessageFlags.Ephemeral
+      });
+    }
+  }
+});
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
