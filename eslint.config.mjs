8f7cbfd0e1f6dfe4236e161068e4e288c8e188f7
jakemackie <jake.wmackie@gmail.com>
2025-02-11 20:53:07 +0000

    amend: only watch ts files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  {ignores: ["dist", "node_modules"]},
+  { files: ['**/*.{ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  { ignores: ['dist', 'node_modules'] },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommended
 ];
